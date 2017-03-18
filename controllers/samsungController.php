139f5d2cea1577182092325bfdd38150585e110f
AndrewTachilin <andrewtachilin@gmail.com>
2017-03-18 17:25:12 +0200

    Update samsungController.php

--- controllers/samsungController.php
+++ controllers/samsungController.php
@@ -4,14 +4,7 @@ include_once(ROOT . '/model/UpdateExistSamsung.php');
 
 class SamsungController extends UpdateExistSamsung
 {
-    public function __construct()
-    {
-        set_error_handler(array($this, 'ErrorCatcher'));
-
-
-        $errorVarArray['real index'] = true;
-        echo $errorVarArray['error index'];
-    }
+7
 
     /**
      * @param $path = path to printer
@@ -23,6 +16,9 @@ class SamsungController extends UpdateExistSamsung
     {
 
         $arrayofSamsungs = [
+
+            'sm2622',
+            'sm2626',
             'sm2752',
             'sm2847',
             'sm2861',
@@ -42,69 +38,59 @@ class SamsungController extends UpdateExistSamsung
             'sm2730',
             'sm6170',
         ];
+
+
         foreach ($arrayofSamsungs as $oneSamsung) {
-            $path = file_get_contents('http://' . $oneSamsung . '/sws/app/information/home/home.json');
+
+         $path = file_get_contents('http://' . $oneSamsung . '/sws/app/information/home/home.json');
+
             $regularExpression = '/remaining:\s([0-9]{1,3})/';
             $check = preg_match_all($regularExpression, $path, $matches);
             if ($check) {
                 $fullname = explode('"', $matches[0][0]);
                 $procentCartridge = explode(" ", $fullname[0]);
-            } else return false;
+            }
             $check = preg_match('/model_name:\s"[a-zA-Z]{1,}\sSCX-[\d]{1,}[a-z]/', $path, $matches);
             if ($check) {
                 $fullname = explode('"', $matches[0]);
                 $nameAndType = explode(" ", $fullname[1]);
                 var_dump($nameAndType);
-            } else return false;
+            }
+
+
+
+
             if (parent::issetPrinter($procentCartridge[1], $nameAndType[0], $oneSamsung)) {
-                echo 'its ok';
-            }
 
+            }
         }
 
 
+
+
     }
-
-    public function ErrorCatcher($errno, $errstr)
-    {
-        echo '<h1>cannot connect to printer </h1>';
-    }
-
-
-    public function actionHp()
-    {
-
-        $arrayofHps = [
-            'hp2472',
-            'hp2635',
-        ];
-        foreach ($arrayofHps as $oneHp) {
-            $path = file_get_contents('http://' . $oneHp . 'info_suppliesStatus.html?tab=Status&menu=SupplyStatus');
-            $regularExpression = '/remaining:\s([0-9]{1,3})/';
-            $check = preg_match_all($regularExpression, $path, $matches);
-            if ($check) {
-                $fullname = explode('"', $matches[0][0]);
-                $procentCartridge = explode(" ", $fullname[0]);
-            } else return false;
-            $check = preg_match('/model_name:\s"[a-zA-Z]{1,}\sSCX-[\d]{1,}[a-z]/', $path, $matches);
-            if ($check) {
-                $fullname = explode('"', $matches[0]);
-                $nameAndType = explode(" ", $fullname[1]);
-                var_dump($nameAndType);
-            } else return false;
-            if (parent::issetPrinter($procentCartridge[1], $nameAndType[0], $oneHp)) {
-                echo 'its ok';
-            }
-
+    public function selectall(){
+        if(parent::select()){
+             return (parent::select());
         }
-
-
     }
+
+
+7
 }
 
 
 
 
 
+
 $samung = new SamsungController();
+//$samung->actionSamsung();
 $samung->actionSamsung();
+//var_dump($samung->selectAll());
+//var_dump($resultSamsung)
+$resultOfall  = $samung->selectall();
+//var_dump($resultOfall);
+foreach ($resultOfall as $all){
+
+}
